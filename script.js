30fff0bcb4e5f36d8da55c81d39bdcd31555af7b
CarolJohn <caroljasmine555@gmail.com>
2022-06-10 02:30:10 +0500

    added files

---
+++ script.js
@@ -0,0 +1,98 @@
+let button1 = document.querySelector("#q1");
+let answer1 = document.querySelector("#answer1");
+let icon1 = document.querySelector("#id1");
+
+button1.onclick = () => {
+  if (answer1.style.display == "none")
+  {
+      answer1.style.display = "block";
+      icon1.classList.toggle("fa-xmark");
+  }
+  else {
+      answer1.style.display = "none";
+      icon1.classList.remove("fa-xmark");
+  }
+};
+
+let button2 = document.querySelector("#q2");
+let answer2 = document.querySelector("#answer2");
+let icon2 = document.querySelector("#id2");
+
+button2.onclick = () => {
+  if (answer2.style.display == "none")
+  {
+      answer2.style.display = "block";
+      icon2.classList.toggle("fa-xmark");
+  }
+  else {
+      answer2.style.display = "none";
+      icon2.classList.remove("fa-xmark");
+  }
+};
+
+let button3 = document.querySelector("#q3");
+let answer3 = document.querySelector("#answer3");
+let icon3 = document.querySelector("#id3");
+
+button3.onclick = () => {
+  if (answer3.style.display == "none")
+  {
+      answer3.style.display = "block";
+      icon3.classList.toggle("fa-xmark");
+  }
+  else {
+      answer3.style.display = "none";
+      icon3.classList.remove("fa-xmark");
+  }
+};
+
+let button4 = document.querySelector("#q4");
+let answer4 = document.querySelector("#answer4");
+let icon4 = document.querySelector("#id4");
+
+button4.onclick = () => {
+  if (answer4.style.display == "none")
+  {
+      answer4.style.display = "block";
+      icon4.classList.toggle("fa-xmark");
+  }
+  else {
+      answer4.style.display = "none";
+      icon4.classList.remove("fa-xmark");
+  }
+};
+
+let button5 = document.querySelector("#q5");
+let answer5 = document.querySelector("#answer5");
+let icon5 = document.querySelector("#id5");
+
+button5.onclick = () => {
+  if (answer5.style.display == "none")
+  {
+      answer5.style.display = "block";
+      icon5.classList.toggle("fa-xmark");
+  }
+  else {
+      answer5.style.display = "none";
+      icon5.classList.remove("fa-xmark");
+  }
+};
+
+let button6 = document.querySelector("#q6");
+let answer6 = document.querySelector("#answer6");
+let icon6 = document.querySelector("#id6");
+
+button6.onclick = () => {
+  if (answer6.style.display == "none")
+  {
+      answer6.style.display = "block";
+      icon6.classList.toggle("fa-xmark");
+  }
+  else {
+      answer6.style.display = "none";
+      icon6.classList.remove("fa-xmark");
+  }
+};
+
+
+
